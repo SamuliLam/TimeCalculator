5e18b1b3c7d22a63a147f3136d175a4a99f6cd7d
Samuli Lamminmäki <36449245+SamuliLam@users.noreply.github.com>
2024-09-23 14:52:45 +0300

    docker task done

---
+++ src/main/java/TimeCalculator.java
@@ -0,0 +1,17 @@
+public class TimeCalculator {
+    public double calculateTime(double distance, double speed) {
+        if (speed <= 0) {
+            throw new IllegalArgumentException("Speed must be greater than zero.");
+        }
+        return distance / speed;
+    }
+
+    public static void main(String[] args) {
+        TimeCalculator timeCalculator = new TimeCalculator();
+        double distance = 100;
+        double speed = 10;
+        double time = timeCalculator.calculateTime(distance, speed);
+        System.out.println("Time taken to travel " + distance + " km at " + speed + " km/h is " + time + " hours.");
+    }
+}
+
